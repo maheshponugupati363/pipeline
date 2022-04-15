ff5027fab1346409d4fd7bb92ce2e40fe8bdfcf5
maheshponugupati363 <99989852+maheshponugupati363@users.noreply.github.com>
2022-04-15 11:55:45 +0530

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -1,15 +1,11 @@
-
 resource "azurerm_virtual_network" "example" {
   name                = var.vnetname
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  address_space       = var.vnetaddress 
-
+  address_space       = var.vnetaddr
   subnet {
     name           = var.subnetname
-    address_prefix = var.subnetaddress 
+    address_prefix = var.subnetaddr
   }
 
  
-  }
-
