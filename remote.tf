5603b5a79ba42d83285034f0ae8b33df8c0d9fd2
maheshponugupati363 <99989852+maheshponugupati363@users.noreply.github.com>
2022-04-15 11:18:29 +0530

    Update remote.tf

--- remote.tf
+++ remote.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    storage_account_name = "mahi00123"
+    storage_account_name = "maheshazure"
     container_name       = "container1"
     key                  = "terraform.tfstate"
     access_key = "Ksg7Q~U4gD7aRerC1as~wpAhZYmH612eItFre"
