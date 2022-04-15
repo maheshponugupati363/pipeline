4cb369f8f6ecc6ed1a9cea16d3ad815d56d017bb
maheshponugupati363 <99989852+maheshponugupati363@users.noreply.github.com>
2022-04-15 11:30:23 +0530

    Update remote.tf

--- remote.tf
+++ remote.tf
@@ -3,6 +3,6 @@ terraform {
     storage_account_name = "maheshazure"
     container_name       = "container1"
     key                  = "terraform.tfstate"
-    access_key = "Ksg7Q~U4gD7aRerC1as~wpAhZYmH612eItFre"
+    access_key = "2rn90jZE3ZgMSjmItJshfRIxd0H095vHem3LinQhBlVpZbuAoQ7WdaaZJm8f6+Uu/ctFWpmKYaYz+AStshbAYw=="
   }
 }
