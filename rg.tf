e9061e569351aa9cfb41b873e49c0a56d876e463
maheshponugupati363 <99989852+maheshponugupati363@users.noreply.github.com>
2022-04-15 14:12:06 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -1,4 +1,21 @@
+
 resource "azurerm_resource_group" "example" {
-  name     = var.rgname
-  location = var.loc
+  name     = "devrg"
+  location = "West Europe"
+}
+resource "azurerm_virtual_network" "example" {
+  name                = "vnet1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["12.0.0.0/16"]
+ 
+  subnet {
+    name           = "subnet1"
+    address_prefix = "12.0.1.0/24"
+  }
+
+ 
+  tags = {
+    environment = "dev"
+  }
 }
